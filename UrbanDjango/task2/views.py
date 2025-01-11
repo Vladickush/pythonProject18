fc30ffac814e772d59255c8c57b258a7c724829b
Vladickush <u.vn@mail.ru>
2025-01-11 14:23:15 +0300

    Update views.py

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def func_templates(request):
-    return render(request, 'func_template.html')
+    return render(request, 'func_template.html')  # 'DIRS': [BASE_DIR / 'templates/second_task'] прописан в setting
     #return render(request, 'second_task/func_template.html')
 
 class ClassTemplates(TemplateView):
-    template_name = 'class_template.html'
+    template_name = 'class_template.html'    ## 'DIRS': [BASE_DIR / 'templates/second_task'] прописан в setting
     #template_name = 'second_task/class_template.html'
